0b95d693d3364781ecab9f5ed56d68fb8fb36555
Eric BREHAULT <ebrehault@gmail.com>
2011-12-14 10:27:53 +0100

    remove old dir

--- plomino.exhibit/plomino/exhibit/interfaces.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from eea.daviz.interfaces import IExhibitJson
-
-class IPlominoExhibitJson(IExhibitJson):
-    """ Plomino objects which have Exhibit Json exported data.
-    """
